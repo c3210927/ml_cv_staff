e19b0d224ddd48338f4371c4d461beca8ba57671
Rahul Paul <rpaul@powercor.com.au>
2024-07-09 15:45:29 +1000

    test updated

--- tests/test_3.py
+++ tests/test_3.py
@@ -1,21 +1,21 @@
-import unittest
+from src import question_4
+import pytest
 
-class TestAnnotationProcessing(unittest.TestCase):
 
-    def setUp(self):
-        pass
+def setUp(self):
+    pass
 
-    def test_load_image(self):
-        pass
+def test_load_image(self):
+    pass
 
-    def test_load_annotation(self):
-        pass
+def test_load_annotation(self):
+    pass
 
-    def test_draw_polygon(self):
-        pass
+def test_draw_polygon(self):
+    pass
 
-    def test_create_mask(self):
-        pass
+def test_create_mask(self):
+    pass
 
 if __name__ == '__main__':
-    unittest.main()
+    pass
